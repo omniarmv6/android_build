928cca87a5c5a0c9035ae8006ff763c2d98a6e96
androidmeda <hephappy@gmail.com>
2014-02-17 17:11:33 +0200

    BML: Remove assert while writing boot.img
    Change-Id: I0efd7c1a8d74e295143534cfe9d9f7449ec54051

--- tools/releasetools/edify_generator.py
+++ tools/releasetools/edify_generator.py
@@ -230,7 +230,7 @@ class EdifyGenerator(object):
       p = fstab[mount_point]
       partition_type = common.PARTITION_TYPES[p.fs_type]
       args = {'device': p.device, 'fn': fn}
-      if partition_type == "MTD":
+      if partition_type == "BML" or partition_type == "MTD":
         self.script.append(
             'package_extract_file("%(fn)s", "/tmp/boot.img");'
             'write_raw_image("/tmp/boot.img", "%(device)s");' % args
@@ -238,11 +238,6 @@ class EdifyGenerator(object):
       elif partition_type == "EMMC":
         self.script.append(
             'package_extract_file("%(fn)s", "%(device)s");' % args)
-      elif partition_type == "BML":
-          self.script.append(
-            ('assert(package_extract_file("%(fn)s", "/tmp/%(device)s.img"),\n'
-             '       write_raw_image("/tmp/%(device)s.img", "%(device)s"),\n'
-             '       delete("/tmp/%(device)s.img"));') % args)
       else:
         raise ValueError("don't know how to write \"%s\" partitions" % (p.fs_type,))
 
